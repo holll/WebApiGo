5117150c685c5a662579f497ebaa0057ede8b122
hollc <380460708@qq.com>
2022-11-11 10:27:41 +0800

    完成消息推送接口，实现区分测试环境和线上环境

--- main.go
+++ main.go
@@ -8,22 +8,26 @@ import (
 )
 
 func main() {
-
-	gin.SetMode(gin.DebugMode) //全局设置环境，此为开发环境，线上环境为gin.ReleaseMode
-	router := gin.Default()    //获得路由实例
-
-	//添加中间件
-	router.Use(Middleware)
-	//注册接口
-	wxPushGroup := router.Group("/send")
-	{
-		wxPushGroup.GET("/", tools.WxPushSendHandler)
+	router := gin.Default() //获得路由实例
+	exists, err := tools.PathExists("debug")
+	if exists && err == nil {
+		gin.SetMode(gin.DebugMode)
+		fmt.Println("当前为测试环境")
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+		fmt.Println("当前为线上环境")
+		//添加中间件
+		router.Use(Middleware)
 	}
-	router.POST("/simple/server/post", PostHandler)
-	router.PUT("/simple/server/put", PutHandler)
-	router.DELETE("/simple/server/delete", DeleteHandler)
+
+	//注册接口
+	wxPushGroup := router.Group("/wxpush")
+	{
+		wxPushGroup.GET("/send", tools.WxPushSendHandler)
+		wxPushGroup.GET("/update", tools.WxPushUpdateHandler)
+	}
 	//监听端口
-	err := http.ListenAndServe(":8005", router)
+	err = http.ListenAndServe(":8005", router)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -32,23 +36,4 @@ func main() {
 
 func Middleware(c *gin.Context) {
 	fmt.Println("this is a middleware!")
-}
-
-func PostHandler(c *gin.Context) {
-	type JsonHolder struct {
-		Id   int    `json:"id"`
-		Name string `json:"name"`
-	}
-	holder := JsonHolder{Id: 1, Name: "my name"}
-	//若返回json数据，可以直接使用gin封装好的JSON方法
-	c.JSON(http.StatusOK, holder)
-	return
-}
-func PutHandler(c *gin.Context) {
-	c.Data(http.StatusOK, "text/plain", []byte("put success!\n"))
-	return
-}
-func DeleteHandler(c *gin.Context) {
-	c.Data(http.StatusOK, "text/plain", []byte("delete success!\n"))
-	return
 }
--- tools/wxPushHandler.go
+++ tools/wxPushHandler.go
@@ -1,38 +1,121 @@
 package tools
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"net/url"
 	"os"
 )
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse and unparse this JSON data, add this code to your project and do:
+//
+//    wxSend, err := UnmarshalWxSend(bytes)
+//    bytes, err = wxSend.Marshal()
+
+func UnmarshalWxSend(data []byte) (WxSend, error) {
+	var r WxSend
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *WxSend) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type WxSend struct {
+	Touser                 string `json:"touser"`
+	Msgtype                string `json:"msgtype"`
+	Agentid                string `json:"agentid"`
+	Text                   Text   `json:"text"`
+	EnableDuplicateCheck   uint8  `json:"enable_duplicate_check"`
+	DuplicateCheckInterval uint16 `json:"duplicate_check_interval"`
+}
+
+type Text struct {
+	Content string `json:"content"`
+}
+
+func UnmarshalWxRep(data []byte) (WxRep, error) {
+	var r WxRep
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *WxRep) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type WxRep struct {
+	Errcode int64  `json:"errcode"`
+	Errmsg  string `json:"errmsg"`
+}
+
+func NewWxSendJson(uid, content, agentId, rawUrl string, check uint8) *WxSend {
+	data := new(WxSend)
+	data.Touser = uid
+	data.Msgtype = "text"
+	data.Agentid = agentId
+	if len(rawUrl) != 0 {
+		content = fmt.Sprintf("%s\n<a href=\"%s\">点击查看原文链接</a>", content, rawUrl)
+	}
+	data.Text = Text{Content: content}
+	data.EnableDuplicateCheck = check
+	data.DuplicateCheckInterval = 900
+	return data
+}
 
 func WxPushSendHandler(c *gin.Context) {
 	// 以下为必须参数
 	uid, _ := c.GetQuery("uid")
 	content, _ := c.GetQuery("content")
-	if len(uid) == 0 || len(content) == 0 {
+	agentId, _ := c.GetQuery("agentid")
+	if len(uid) == 0 || len(content) == 0 || len(agentId) == 0 {
 		c.JSON(http.StatusOK, gin.H{"code": 502, "msg": "缺少必须参数"})
+		return
 	}
 	// 以下为可选参数
-	//url, _ := c.GetQuery("url")
-	//cache, _ := c.GetQuery("cache")
+	tmpCache, _ := c.GetQuery("cache")
+	check := uint8(1)
+	if len(tmpCache) != 0 && tmpCache == "0" {
+		check = 0
+	}
+	rawUrl, _ := c.GetQuery("url")
 	tmpAccessToken, err := os.ReadFile("a.txt")
 	if err != nil {
-		panic(err)
+		c.JSON(http.StatusOK, gin.H{"code": 502, "msg": "读取AccessToken失败"})
+		return
 	}
 	accessToken := string(tmpAccessToken)
-	params := url.Values{}
-	Url, _ := url.Parse("https://qyapi.weixin.qq.com/cgi-bin/message/send")
-	params.Set("access_token", accessToken)
-	Url.RawQuery = params.Encode()
-	urlPath := Url.String()
-	rep, err := http.Get(urlPath)
-	if err != nil || rep.StatusCode != 200 {
-		c.JSON(http.StatusOK, gin.H{"code": 501, "msg": "发送失败"})
-	} else {
-		c.JSON(http.StatusOK, gin.H{"code": 200, "msg": "发送成功"})
-		fmt.Println(RepBodyToStr(rep.Body))
+	data := NewWxSendJson(uid, content, agentId, rawUrl, check)
+	marshal, err := data.Marshal()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"code": 502, "msg": "参数格式化失败，请检测数据合法性"})
+		return
 	}
+	rep, err := http.Post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="+accessToken, "application/json", bytes.NewReader(marshal))
+	if err != nil || rep.StatusCode != 200 {
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{"code": 501, "msg": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"code": 501, "msg": "状态码为" + rep.Status})
+		}
+	} else {
+		wxRep, err := UnmarshalWxRep(RepBodyToByteSlice(rep.Body))
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{"code": 504, "msg": "请求已发送，但解析响应数据失败"})
+			return
+		}
+		if wxRep.Errcode != 0 {
+			c.JSON(http.StatusOK, gin.H{"code": 505, "msg": wxRep.Errmsg})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"code": 200, "msg": "发送成功"})
+	}
+}
+
+func WxPushUpdateHandler(c *gin.Context) {
+
 }
